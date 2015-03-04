792be064d086bd4439cd5f4c81b12b370f1d0729
OstrichRun <347647092@qq.com>
2015-03-04 15:31:18 +0800

    iOS
    拆分

---
+++ src/iOS/DoExt_Camera_SM/ext_implement/Do_Camera_SM.h
@@ -0,0 +1,14 @@
+//
+//  TYPEID_SM.h
+//  DoExt_API
+//
+//  Created by @userName on @time.
+//  Copyright (c) 2015年 DoExt. All rights reserved.
+//
+
+#import "Do_Camera_ISM.h"
+#import "doSingletonModule.h"
+
+@interface Do_Camera_SM : doSingletonModule<Do_Camera_ISM>
+
+@end
